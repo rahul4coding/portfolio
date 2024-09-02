ca248f9281caecd4e2513b74a52ef3bd4aa8803c
rahul4coding <rahulbhatija123@gmail.com>
2024-09-03 02:15:44 +0530

    resume updated @ europass

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Resume.pdf";
+import pdf from "../../Assets/../Assets/rahul-bhatija-cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -36,10 +36,7 @@ function ResumeNew() {
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
             
           </Document>
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
-            
-          </Document>
+          
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
